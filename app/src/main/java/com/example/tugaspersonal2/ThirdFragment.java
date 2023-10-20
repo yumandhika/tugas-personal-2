612efd31e1b3d95a6521492a6d3ca0311d610cf0
Yuma Andhika <yumaandhika@Yumas-MacBook-Pro.local>
2023-10-21 00:05:42 +0700

    add maps

--- app/src/main/java/com/example/tugaspersonal2/ThirdFragment.java
+++ app/src/main/java/com/example/tugaspersonal2/ThirdFragment.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.CameraUpdateFactory;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +19,8 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class ThirdFragment extends Fragment {
+
+    private GoogleMap googleMap;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -49,16 +56,22 @@ public class ThirdFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_third, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_third, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(gMap -> {
+            googleMap = gMap;
+            if (googleMap != null) {
+                // Set lokasi ke Bandung
+                LatLng bandung = new LatLng(-6.2088, 106.8456);
+                googleMap.addMarker(new MarkerOptions().position(bandung).title("Bandung"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(bandung, 13));
+            }
+
+        });
+        return view;
     }
 }
